62cfb0c46e0f82d6b4e1b2abf743e3f4d3d5aa90
pswbuild <pswbuild@oppo.com>
2024-07-01 17:59:34 +0800

    Synchronize code for OPPO OPD2302_14.0.0.801(EX01) Based on MTK release TAG:t-alps-release-s0.mp1.rc-V14.75

--- drivers/media/platform/mtk-vcodec/vdec/vdec_vcp_if.c
+++ drivers/media/platform/mtk-vcodec/vdec/vdec_vcp_if.c
@@ -26,6 +26,7 @@
 #else
 #define IPI_TIMEOUT_MS          (5000U + ((mtk_vcodec_dbg | mtk_v4l2_dbg_level) ? 5000U : 0U))
 #endif
+#define IPI_FIRST_DEC_START_TIMEOUT_MS     (60000U)
 
 struct vcp_dec_mem_list {
 	struct vcodec_mem_obj mem;
@@ -163,6 +164,9 @@ static int vdec_vcp_ipi_send(struct vdec_inst *inst, void *msg, int len, bool is
 
 	obj.len = len;
 	ipi_size = ((sizeof(u32) * 2) + len + 3) /4;
+	inst->vcu.failure = 0;
+	inst->ctx->err_msg = 0;
+
 	if (!is_ack) {
 		*msg_signaled = false;
 		if (!is_res)
@@ -184,13 +188,18 @@ static int vdec_vcp_ipi_send(struct vdec_inst *inst, void *msg, int len, bool is
 		inst->vcu.abort = 1;
 		if (inst->vcu.daemon_pid == get_vcp_generation())
 			trigger_vcp_halt(VCP_A_ID);
+		inst->ctx->err_msg = *(__u32 *)msg;
 		return -EIO;
 	}
 
 	if (!is_ack) {
 wait_ack:
 		/* wait for VCP's ACK */
-		timeout = msecs_to_jiffies(IPI_TIMEOUT_MS);
+		if (*(__u32 *)msg == AP_IPIMSG_DEC_START && inst->ctx->state == MTK_STATE_INIT)
+			timeout = msecs_to_jiffies(IPI_FIRST_DEC_START_TIMEOUT_MS);
+		else
+			timeout = msecs_to_jiffies(IPI_TIMEOUT_MS);
+
 		ret = wait_event_timeout(*msg_wq, *msg_signaled, timeout);
 		*msg_signaled = false;
 
@@ -202,6 +211,7 @@ wait_ack:
 			inst->vcu.abort = 1;
 			if (inst->vcu.daemon_pid == get_vcp_generation())
 				trigger_vcp_halt(VCP_A_ID);
+			inst->ctx->err_msg = *(__u32 *)msg;
 			return -EIO;
 		} else if (-ERESTARTSYS == ret) {
 			mtk_vcodec_err(inst, "wait vcp ipi %X ack ret %d RESTARTSYS retry! (%d)",
@@ -772,6 +782,7 @@ static int vcp_vdec_notify_callback(struct notifier_block *this,
 	struct mtk_vcodec_ctx *ctx;
 	int timeout = 0;
 	bool backup = false;
+	struct vdec_inst *inst = NULL;
 
 	if (!(mtk_vcodec_vcp & (1 << MTK_INST_DECODER)))
 		return 0;
@@ -794,6 +805,11 @@ static int vcp_vdec_notify_callback(struct notifier_block *this,
 			ctx = list_entry(p, struct mtk_vcodec_ctx, list);
 			if (ctx != NULL && ctx->state != MTK_STATE_ABORT) {
 				ctx->state = MTK_STATE_ABORT;
+				inst = (struct vdec_inst *)(ctx->drv_handle);
+				if (inst != NULL) {
+					inst->vcu.failure = VDEC_IPI_MSG_STATUS_FAIL;
+					inst->vcu.abort = 1;
+				}
 				vdec_check_release_lock(ctx);
 				mtk_vdec_queue_error_event(ctx);
 			}
